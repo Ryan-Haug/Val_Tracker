c1306d41bd99ea9a10443ad250eae89091570dd8
Ryan-Haug-Student <ryha601@student.oaisd.org>
2026-04-14 14:39:27 -0400

    adding card template

---
+++ scripts/main.js
@@ -0,0 +1,155 @@
+const testData = {
+  "status": 200,
+  "name": "Chos3n",
+  "tag": "50hs",
+  "data": [
+    {
+      "currenttier": 21,
+      "currenttierpatched": "Ascendant 1",
+      "images": {
+        "small": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/smallicon.png",
+        "large": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/largeicon.png",
+        "triangle_down": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/ranktriangledownicon.png",
+        "triangle_up": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/ranktriangleupicon.png"
+      },
+      "match_id": "23c2d421-c6f1-4efd-a675-960a080d5e5e",
+      "map": {
+        "id": "2c9d57ec-4431-9c5e-2939-8f9ef6dd5cba",
+        "name": "Bind"
+      },
+      "season_id": "9d85c932-4820-c060-09c3-668636d4df1b",
+      "ranking_in_tier": 29,
+      "mmr_change_to_last_game": 17,
+      "elo": 1829,
+      "date": "Tuesday, April 14, 2026 02:28 AM",
+      "date_raw": 1776133725
+    },
+    {
+      "currenttier": 21,
+      "currenttierpatched": "Ascendant 1",
+      "images": {
+        "small": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/smallicon.png",
+        "large": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/largeicon.png",
+        "triangle_down": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/ranktriangledownicon.png",
+        "triangle_up": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/ranktriangleupicon.png"
+      },
+      "match_id": "4975a8ba-2d99-4745-a62c-07dcc20fec57",
+      "map": {
+        "id": "2fb9a4fd-47b8-4e7d-a969-74b4046ebd53",
+        "name": "Breeze"
+      },
+      "season_id": "9d85c932-4820-c060-09c3-668636d4df1b",
+      "ranking_in_tier": 12,
+      "mmr_change_to_last_game": -17,
+      "elo": 1812,
+      "date": "Tuesday, April 14, 2026 01:53 AM",
+      "date_raw": 1776131601
+    },
+    {
+      "currenttier": 21,
+      "currenttierpatched": "Ascendant 1",
+      "images": {
+        "small": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/smallicon.png",
+        "large": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/largeicon.png",
+        "triangle_down": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/ranktriangledownicon.png",
+        "triangle_up": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/ranktriangleupicon.png"
+      },
+      "match_id": "07bb6576-02d4-43cf-9bdc-5097cb867369",
+      "map": {
+        "id": "fd267378-4d1d-484f-ff52-77821ed10dc2",
+        "name": "Pearl"
+      },
+      "season_id": "9d85c932-4820-c060-09c3-668636d4df1b",
+      "ranking_in_tier": 29,
+      "mmr_change_to_last_game": 18,
+      "elo": 1829,
+      "date": "Sunday, April 12, 2026 05:01 PM",
+      "date_raw": 1776013268
+    },
+    {
+      "currenttier": 21,
+      "currenttierpatched": "Ascendant 1",
+      "images": {
+        "small": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/smallicon.png",
+        "large": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/largeicon.png",
+        "triangle_down": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/ranktriangledownicon.png",
+        "triangle_up": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/ranktriangleupicon.png"
+      },
+      "match_id": "2059680a-5333-4506-9ede-63d0b10aa9ca",
+      "map": {
+        "id": "2c9d57ec-4431-9c5e-2939-8f9ef6dd5cba",
+        "name": "Bind"
+      },
+      "season_id": "9d85c932-4820-c060-09c3-668636d4df1b",
+      "ranking_in_tier": 11,
+      "mmr_change_to_last_game": -16,
+      "elo": 1811,
+      "date": "Sunday, April 05, 2026 01:00 AM",
+      "date_raw": 1775350855
+    },
+    {
+      "currenttier": 21,
+      "currenttierpatched": "Ascendant 1",
+      "images": {
+        "small": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/smallicon.png",
+        "large": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/largeicon.png",
+        "triangle_down": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/ranktriangledownicon.png",
+        "triangle_up": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/ranktriangleupicon.png"
+      },
+      "match_id": "075b5b1d-06dd-446d-99b4-4ef8a377d167",
+      "map": {
+        "id": "2bee0dc9-4ffe-519b-1cbd-7fbe763a6047",
+        "name": "Haven"
+      },
+      "season_id": "9d85c932-4820-c060-09c3-668636d4df1b",
+      "ranking_in_tier": 27,
+      "mmr_change_to_last_game": 19,
+      "elo": 1827,
+      "date": "Saturday, April 04, 2026 05:29 PM",
+      "date_raw": 1775323749
+    },
+    {
+      "currenttier": 21,
+      "currenttierpatched": "Ascendant 1",
+      "images": {
+        "small": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/smallicon.png",
+        "large": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/largeicon.png",
+        "triangle_down": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/ranktriangledownicon.png",
+        "triangle_up": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/ranktriangleupicon.png"
+      },
+      "match_id": "977a2a39-9809-4029-a8c7-a8d664458613",
+      "map": {
+        "id": "d960549e-485c-e861-8d71-aa9d1aed12a2",
+        "name": "Split"
+      },
+      "season_id": "9d85c932-4820-c060-09c3-668636d4df1b",
+      "ranking_in_tier": 8,
+      "mmr_change_to_last_game": -17,
+      "elo": 1808,
+      "date": "Saturday, April 04, 2026 04:41 PM",
+      "date_raw": 1775320884
+    },
+    {
+      "currenttier": 21,
+      "currenttierpatched": "Ascendant 1",
+      "images": {
+        "small": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/smallicon.png",
+        "large": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/largeicon.png",
+        "triangle_down": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/ranktriangledownicon.png",
+        "triangle_up": "https://media.valorant-api.com/competitivetiers/03621f52-342b-cf4e-4f86-9350a49c6d04/21/ranktriangleupicon.png"
+      },
+      "match_id": "22af7b2b-7293-4775-bbac-e1cb2e8b9452",
+      "map": {
+        "id": "2fb9a4fd-47b8-4e7d-a969-74b4046ebd53",
+        "name": "Breeze"
+      },
+      "season_id": "9d85c932-4820-c060-09c3-668636d4df1b",
+      "ranking_in_tier": 25,
+      "mmr_change_to_last_game": -18,
+      "elo": 1825,
+      "date": "Saturday, April 04, 2026 01:37 AM",
+      "date_raw": 1775266637
+    }
+  ]
+}
+
